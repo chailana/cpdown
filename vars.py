912a4ecc3299c7f405bb25016bb212efe2c8feef
chailana <chailana35217793@gmail.com>
2024-10-28 21:44:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
-api_id =  #change_with_your_Api_id.
-api_hash = "" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id =  22420997 #change_with_your_Api_id.
+api_hash = "d7fbe2036e9ed2a1468fad5a5584a255" #change_with_your_Api_hash.
+bot_token = "7176424785:AAEusrLtmtGgRisJ6Pje6yAnN-ZbdZMoO1Q" #change_with_your_bot_token.
+auth_users = [7170426058] #change_with_your_user_id.
+sudo_user = 7170426058 #change_with_your_user_id.
+log_channel = -1002247666039 #change_with_your_channel_id.
+txt_channel = -1002247666039 #change_with_your_channel_id.
